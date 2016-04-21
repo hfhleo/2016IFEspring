c01d73168c2978fdab8dff8a0ebfafe8bb4fccad
shanlanmi <363207264@163.com>
2016-04-21 11:10:34 +0800

    #

--- task/task2.24/task.js
+++ task/task2.24/task.js
@@ -1,15 +1,20 @@
 // 全局参数：速度，算法，动画状态, 动画信息寄存器
 var speed = document.getElementById("interval");
 var arithmetic = document.getElementById("arithmetic");
-var root = document.getElementById("wrapTree").firstElementChild;
 var searchInput = document.getElementById("searchInput");
-var traversOne = new traversing();
+var addInput = document.getElementById("addInput");
+var oneTree = new tree();
 
+/*
+ * * * 事件相关 * */
 // 事件绑定
 function addEvent() {
   document.getElementById("generation").onclick = generation;
   document.getElementById("start").onclick = startShow;
   document.getElementById("searchStart").onclick = search;
+  document.getElementById("wrapTree").addEventListener("click", clickTree);
+  document.getElementById("add").onclick = add;
+  document.getElementById("del").onclick = del;
 }
 
 // 随机生成多叉树
@@ -35,7 +40,6 @@ function generation() {
   function genBranch(node, n) {
     if (n === 0 ) return;
     var howMany = random(1,3);
-    var genTimes = 0;
     var insert = ranLetter(2);
 
     // 生成下一层分支
@@ -51,50 +55,84 @@ function generation() {
     }
   }
 
-  var oneNode = document.getElementById("root");
-  genBranch(oneNode, 3);
+  var oneNode = document.getElementById("wrapTree");
+  // 生成 roo 节点
+  oneNode.innerHTML = "<div></div>";
+  genBranch(oneNode.childNodes[0], 3);
 }
 
 // 开始历遍按钮
 function startShow() {
-  if (traversOne.inAnimation) {
+  if (oneTree.inAnimation) {
     alert("演示中，请稍等");
     return;
   }
-  traversOne.forShow = [];
-  traversOne.inAnimation = true;
+  oneTree.forShow = [];
+  oneTree.inAnimation = true;
+  var root = document.getElementById("wrapTree").firstElementChild;
   switch (arithmetic.value) {
     case "1":
-      traversOne.fontOrder(root);
+      oneTree.fontOrder(root);
       break;
     default:
-      traversOne.preOrder(root);
+      oneTree.preOrder(root);
   }
-  traversOne.forShow.push("");
-  traversOne.show(speed.value);
+  oneTree.forShow.push("");
+  oneTree.show(speed.value);
 }
 
 // 查询按钮
 function search() {
-  if (traversOne.inAnimation) {
+  if (oneTree.inAnimation) {
     alert("演示中，请稍等");
     return;
   }
-  traversOne.forShow = [];
-  traversOne.searched = [];
-  traversOne.search(root);
-  traversOne.show(speed.value, true);
+  var root = document.getElementById("wrapTree").firstElementChild;
+  oneTree.forShow = [];
+  oneTree.searched = [];
+  oneTree.search(root);
+  oneTree.show(speed.value, true);
 }
 
+// 点击节点事件
+function clickTree(x) {
+  var tar = x.target;
+  if (tar.nodeName === "DIV") {
+    if (tar.className === "selected") {
+      tar.style.backgroundColor = "";
+      tar.className = "";
+    } else {
+      tar.style.backgroundColor = "lightblue";
+      tar.className = "selected";
+    }
+  }
+}
+
+// 添加按钮事件
+function add() {
+  var addValue = addInput.value;
+  var addTar = document.getElementsByClassName("selected");
+  oneTree.add(addTar,addValue);
+}
+
+
+// 删除按钮事件
+function del() {
+  var addTar = document.getElementsByClassName("selected");
+  oneTree.del(addTar);
+}
+
+/*
+   * * * 多叉树 protoptype * */
 // 定义历遍原形
-function traversing() {
+function tree() {
   this.inAnimation = false;
   this.forShow = [];  //记录当前历遍 node
   this.searched = []; // 记录被查询到的匹配 node
 }
 
-// 前序历遍：传出 traversOne.forShow
-traversing.prototype.preOrder = function(node) {
+// 前序历遍：传出 oneTree.forShow
+tree.prototype.preOrder = function(node) {
   if (node !== null) {
     if (node.nodeName === "DIV") {
       this.forShow.push([node]);
@@ -107,8 +145,8 @@ traversing.prototype.preOrder = function(node) {
   }
 };
 
-// 后序历遍：传出 traversOne.forShow
-traversing.prototype.fontOrder = function(node) {
+// 后序历遍：传出 oneTree.forShow
+tree.prototype.fontOrder = function(node) {
   if (node !== null) {
     var children = node.childNodes;
     for (var j = children.length - 1; j > 0; j = j - 1) {
@@ -121,8 +159,8 @@ traversing.prototype.fontOrder = function(node) {
   }
 };
 
-// 历遍查询：传出 traversOne.forShow
-traversing.prototype.search = function(node) {
+// 历遍查询：传出 oneTree.forShow
+tree.prototype.search = function(node) {
   var searchValue = searchInput.value;
   var len = this.forShow.length;
 
@@ -142,7 +180,7 @@ traversing.prototype.search = function(node) {
 };
 
 // 展示动画: 传入(speed[integer])
-traversing.prototype.show = function(aniSpeed, n) {
+tree.prototype.show = function(aniSpeed, n) {
   var self = this;
   var timer = setInterval(show,(aniSpeed || 500));
   function show() {
@@ -165,6 +203,31 @@ traversing.prototype.show = function(aniSpeed, n) {
     }
   }
 };
+
+// 节点添加：传入(元素[nodeList]，节点值[string])
+tree.prototype.add = function(nodeList,str) {
+  var len = nodeList.length;
+  [].forEach.call(nodeList, function(x) {
+    x.innerHTML += "<div>" + str + "<\/div>"; 
+  });
+  for (var i = 0; i < len; i++) {
+    nodeList[0].style.backgroundColor = "";
+    nodeList[0].className= "";
+  }
+};
+
+// 删除节点： 传入(节点元素[nodeList])
+tree.prototype.del = function(nodeList) {
+  var len = nodeList.length;
+  // 有没有不这么蛋疼的删除节点方法？
+  for (var i = 0; i < len; i++) {
+    nodeList[0].parentNode.removeChild(nodeList[0]);
+  }
+};
+
+
+/*
+   * * * 流程控制 * */
 
 // 自启流程控制
 function init() {
